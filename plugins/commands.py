04cb4abceadcd0ae3b732e48d24bacf0bd4b18a7
dmdipanshu <112119455+dmdipanshu@users.noreply.github.com>
2024-01-19 22:47:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -430,7 +430,7 @@ async def start(client, message):
             btn = [[
                 InlineKeyboardButton("Get File Again", callback_data=f'delfile#{file_id}')
             ]]
-            k = await msg.reply("<b><u>,quote=False)
+            k = await msg.reply("<b><u>@Rasmalai_collection,quote=False)
             await asyncio.sleep(600)
             await msg.delete()
             await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
